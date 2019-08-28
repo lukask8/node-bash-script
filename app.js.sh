a90c67e5189198a252e29ff52f3d34d7ebd7e5cf
lukask8 <28091459+lukask8@users.noreply.github.com>
2019-08-28 09:24:36 +0200

    Create app.js.sh

---
+++ app.js.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/** ****************************************************************************
+ * Main app for js. This is an usefull JS with "sh" payload to autostart node
+ * On linux just add an execute permission to this file with chmod +x app.js.sh.
+ * You can now run this node script as you need!
+ * 
+ * $ node app.js.sh
+ * $ ./app.js.sh
+ * ****************************************************************************/
+ "use strict"
+ 
+ console.log("I'm here!");
